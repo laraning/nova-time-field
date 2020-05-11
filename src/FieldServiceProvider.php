e1e5d983e9b4ec3d6368c0eb4ea958617a1e5774
brunocfalcao <bruno.c.falcao@brunofalcao.me>
2020-05-11 14:17:42 +0200

    Updated changes to codebase

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -17,7 +17,6 @@ class FieldServiceProvider extends ServiceProvider
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('nova-time-field', __DIR__.'/../dist/js/field.js');
-            Nova::style('nova-time-field', __DIR__.'/../dist/css/field.css');
         });
     }
 
---
+++ src/NovaTimeField.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Laraning\NovaTimeField;
+
+use Laravel\Nova\Fields\Field;
+
+class NovaTimeField extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'nova-time-field';
+}
