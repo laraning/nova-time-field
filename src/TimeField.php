f0d0d29c1b717a260e9e26a70068a912fd1706d8
brunocfalcao <brunocfalcao@users.noreply.github.com>
2021-05-05 08:24:20 +0000

    Apply fixes from StyleCI

--- src/TimeField.php
+++ src/TimeField.php
@@ -29,13 +29,12 @@ class TimeField extends Field
     public function __construct($name, $attribute = null, $resolveCallback = null)
     {
         parent::__construct($name, $attribute, $resolveCallback ?? function ($value) {
-
-            if (! is_null($value)) {
+            if (!is_null($value)) {
                 // Convert the value string into a Carbon date/time object.
                 $value = Carbon::createFromFormat('H:i:s', $value)->format($this->format());
 
                 if (!$value) {
-                    throw new Exception("Field " . ($attribute ?? '') . " must contain a valid time value.");
+                    throw new Exception('Field '.($attribute ?? '').' must contain a valid time value.');
                 }
 
                 return $value;
@@ -45,6 +44,7 @@ class TimeField extends Field
 
     /**
      * Validates if the 12h format value is a 12h format.
+     *
      * @return bool
      */
     public function format()
@@ -53,8 +53,10 @@ class TimeField extends Field
     }
 
     /**
-     * Defines the minute increment step
+     * Defines the minute increment step.
+     *
      * @param type $step
+     *
      * @return void
      */
     public function minuteIncrement($step)
@@ -64,6 +66,7 @@ class TimeField extends Field
 
     /**
      * Applies 12h format in the field.
+     *
      * @return void
      */
     public function withTwelveHourTime()
@@ -73,6 +76,7 @@ class TimeField extends Field
 
     /**
      * @param null|string $adjustment The adjustment to be applied to the date from the database.
+     *
      * @return TimeField
      */
     public function withTimezoneAdjustments(int $adjustment)
@@ -86,10 +90,11 @@ class TimeField extends Field
     /**
      * Hydrate the given attribute on the model based on the incoming request.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-     * @param  string  $requestAttribute
-     * @param  object  $model
-     * @param  string  $attribute
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @param string                                  $requestAttribute
+     * @param object                                  $model
+     * @param string                                  $attribute
+     *
      * @return mixed
      */
     protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
@@ -107,21 +112,22 @@ class TimeField extends Field
 
             // Call parent method from the Nova framework.
             parent::fillAttributeFromRequest($request, $requestAttribute, $model, $attribute);
-        };
+        }
     }
 
     /**
      * Validate format of given time and return correct format.
      *
      * @param string $timeString
+     *
      * @return mixed
      */
     protected function validatedTimeFormat($timeString)
     {
         $allowedFormats = [
-        'H:i',
-        'H:i:s',
-        $this->format(),
+            'H:i',
+            'H:i:s',
+            $this->format(),
         ];
 
         foreach ($allowedFormats as $format) {
